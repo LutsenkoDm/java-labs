33d8614898c3cdda2f63aa6d92babb98f0590034
Dmitrii Lutsenko <lutsenko.dyu@edu.spbstu.ru>
2020-04-29 00:20:25 +0300

    add lab5 implementation

---
+++ lab5/src/spbstu/ru/Item.java
@@ -0,0 +1,48 @@
+package spbstu.ru;
+
+public class Item
+{
+    private static int idValue = 0;
+    private int id;
+    private int prodid;
+    private String title;
+    private int cost;
+
+    public Item(String title, int cost)
+    {
+        id = ++idValue;
+        prodid = idValue;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public int getProdid()
+    {
+        return prodid;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public int getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(int cost)
+    {
+        this.cost = cost;
+    }
+}
